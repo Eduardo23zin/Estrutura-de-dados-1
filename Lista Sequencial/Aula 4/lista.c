87e8c25b228e0167ac13c72ec9c277dcf10f9200
Eduardo23zin <duducarlos668@gmail.com>
2025-08-28 18:10:26 -0400

    Atualizações, melhorias e identações

--- Lista Sequencial/Aula 4/lista.c
+++ Lista Sequencial/Aula 4/lista.c
@@ -10,87 +10,107 @@ int Cheia(Lista *L){
 }
 int Vazia(Lista *L){
 	return (L->nelem ==0);
-	printf("A lista ta vazia");
-	return 1;
 }
 int InserirOrdenada(Lista *L, tipo_elem v){
-	int i;
+	int i = L->nelem;
 	
 	if(Cheia (L)){
 		return 0;
 	}
-	if(Vazia(L)){
-		return(InserirInicio(L, v));
-	}
 	
-	for(i=L->nelem; i>0; i--){
-		v.chave<L->A[i-1].chave;
+	while(i>0 && v.chave < L->A[i-1].chave){
+		//se ascondições forem verdade, L->A[i-1] é maior que v e tem que se deslocar para a direita
 		L->A[i] = L->A[i-1];
-		L->A[i] = v;
-		L->nelem++;
-		return 1;
+		i--; //movendo o espaço vazio uma posição para a esquerda para continuar a verificação
+		
 	}
+	L->A[i] = v; //achou a posição certa
+	L->nelem++; //atualizando o tamanho da lista
+	return 1;
 }
 
 int InserirInicio(Lista *L, tipo_elem v){
-	int i;
 	
 	if(Cheia(L)){
 		return 0;
 	}
-	for(i=L->nelem; i>0; i--){
+	
+	//reutilizando a função de inserir ordenada para evitar problemas e manter a ordem.
+	return(InserirOrdenada(L, v));
+	
+	//inserir inicio
+	/*for(i=L->nelem; i>0; i--){
 		L->A[i] = L->A[i-1];
 	}
 	
 	L->A[0] = v;
 	L->nelem++;
-	return 1;
+	return 1; */
 }
 
 int InserirFinal(Lista *L,  tipo_elem v){
+
 	if(Cheia(L))
 	return 0;
 	
+	//reutilizando a função de inserir ordenada para evitar problemas e manter a ordem.
+	return(InserirOrdenada(L, v));
+	
+	/*//inserir no final
 	L->A[L->nelem] = v;
 	L->nelem++;
-	return 1;
+	return 1;*/
 }
+
 int RemoverInicio(Lista *L){
 	int i;
-	if(Vazia(L))
-	return 0;
+	tipo_elem elementoRemovido; //variavel temporaria que guarda a primeira posição do vetor para depois imprimir o valor removido
+	if(Vazia(L)){
+		return 0;
+	}
+	elementoRemovido = L->A[0]; //atribuindo o valor da posicao 0
+	
+	//diminuindo o tamanho da lista	
 	L->nelem--;
-	for(i=0; i<L->nelem; i++)
+	for(i=0; i<L->nelem; i++){
 	L->A[i] = L->A[i+1];
+	}
+	printf("O elemento %d foi removido com sucesso\n", elementoRemovido.chave);
 	return 1;
 	
 }
+
 int RemoverFinal(Lista *L){
-	int i;
 	if(Vazia(L)){
 		return 0;
 	}else{
-		L->nelem--;
-		printf("O elemento %d foi removido com sucesso\n", L->A[i].chave);
+		L->nelem--; //diminuindo o tamanho da lista
+		printf("O elemento %d foi removido com sucesso\n", L->A[L->nelem].chave);
 		return 1;
 	}
 }
 
 int Remover(Lista *L, int chave){
 	int i, k;
+	k = Busca(L, chave); //reutilizando a função de busca
+
+	if(Vazia(L)){
+		return 0;
+	}
 	
-	if(Vazia(L))
-	return 0;
+	if(k == -1){
+		printf("Elemento não encotrado.\n");
+		return 0; // elemento não encontrado
+	}else{
+		//deslocamento
+		for(i=k; i<L->nelem-1; i++){
+			L->A[i] = L->A[i+1];
+		}
+	}
 	
-	//Buscar o elemento a ser removido
+	printf("Elemento '%d', removido com sucesso!\n", chave);
 	
-	for(k=0; k<L->A[k].chave && chave!=L->A[k].chave; k++);
-	if(k==L->nelem)
-	return 0; //Não achou
-	
-	for(i=k; i<L->nelem-1; i++);
-	L->A[i] = L->A[i+1];
-	L->nelem--;
+	L->nelem--; //diminuindo/atualizando o tamanho da lista
 	return 1;	
 	
 }
@@ -98,10 +118,14 @@ int Remover(Lista *L, int chave){
 int Busca(Lista *L, int chave){
 	int i;
 	
-	if(Vazia(L))
-	return 0;
-	for(i=0; i<L->nelem && L->A[i].chave!= chave; i++)
-	return (i<L->nelem);
+	for(i=0; i<L->nelem && L->A[i].chave!= chave; i++);
+	
+	if(i<L->nelem){
+		return i; //ecnontrou a chave e retorna a posição;
+	}else{
+		return -1; //não encontrou a chave
+	}
+
 }
 
 
@@ -114,7 +138,7 @@ void Exibir(Lista *L){
 	int i;
 	
 	if(Vazia(L)){
-		printf("Lista vazia\n");
+		printf("Lista vazia, nada para exibir. \n");
 	}else{
 		for(i=0; i<L->nelem; i++){
 			printf("%d ", L->A[i].chave);
@@ -124,6 +148,7 @@ void Exibir(Lista *L){
 }
 
 int Tamanho(Lista *L){
+	printf("A lista contem %d elementos \n", L->nelem);
 	return (L->nelem);
 }
 
@@ -134,14 +159,17 @@ int BuscaBinaria(Lista *L, int chave){
 	while(fim>=inicio){
 		meio =(fim+inicio)/2;
 		
-		if(L->A[meio].chave==chave)
-		return 1;
+		//encontrou, fim da função
+		if(L->A[meio].chave==chave){
+			return 1;
+		}
 		
-		if(L->A[meio].chave>chave)
-		fim = meio-1;
-		
-		else
-			inicio = meio+1;
-			return 0;
+		if(L->A[meio].chave>chave){
+			fim = meio-1; //descarta a metade direita do vetor
+		}else{
+			inicio = meio+1; //descarta a metade esquerda do vetor
+		}
 	}
+	
+	return 0;
 }
